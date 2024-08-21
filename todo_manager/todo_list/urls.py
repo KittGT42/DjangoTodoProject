412e5d08ed85a42c7ef741119f5a1f183a9a8c2e
KittGT42 <114111458+KittGT42@users.noreply.github.com>
2024-08-21 14:54:42 +0300

    All done

--- todo_manager/todo_list/urls.py
+++ todo_manager/todo_list/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import index_view
+from .views import TodoListIndexView, ToDoListView, ToDoListDoneView, ToDoDetailView
 
 app_name = 'todo_list'
 
 urlpatterns = [
-    path('', index_view, name='index'),
+    path('<int:pk>/', ToDoDetailView.as_view(), name='detail'),
+    path('', TodoListIndexView.as_view(), name='index'),
+    path('list/', ToDoListView.as_view(), name='list'),
+    path('done/', ToDoListDoneView.as_view(), name='done'),
+
 ]
--- todo_manager/todo_list/views.py
+++ todo_manager/todo_list/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+
 from .models import ToDoItem
 
 
-def index_view(request: HttpRequest) -> HttpResponse:
-    todo_items = ToDoItem.objects.all()
-    return render(request, 'todo_list/index.html', context={'todo_items': todo_items})
+class TodoListIndexView(ListView):
+    template_name = "todo_list/index.html"
+    queryset = ToDoItem.objects.all()[:1]
+
+
+class ToDoListView(ListView):
+    model = ToDoItem
+
+
+class ToDoListDoneView(ListView):
+    queryset = ToDoItem.objects.filter(done=True)
+
+class ToDoDetailView(DetailView):
+    model = ToDoItem
